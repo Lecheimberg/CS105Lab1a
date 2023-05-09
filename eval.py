95d9b27bf592f611942aed35bdca0638b6d2b6a3
Ismatulla Mansurov <47342870+sapoepsilon@users.noreply.github.com>
2023-05-09 11:37:14 -0600

    fix

--- eval.py
+++ eval.py
@@ -1,20 +1,40 @@
 import subprocess
+import os
+
+
+def remove_main(file_path):
+    try:
+        os.remove(file_path)
+        print(f"File deleted successfully: {file_path}")
+    except OSError as e:
+        print(f"Error deleting file: {e.filename} - {e.strerror}")
+
 
 def run_job(cmd):
-    ret = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    return ret.stdout.decode(('UTF-8'))
+    current_directory = os.getcwd()
+    ret = subprocess.run(
+        cmd,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        cwd=current_directory,
+    )
+    return ret.stdout.decode(("UTF-8"))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Compile
     run_job("javac src/Main.java")
 
     # Run
-    msg = run_job("java src/Main").rstrip()
-    expected_output = "Hello World!"
+    msg = run_job("java -cp src Main").rstrip()
+    expected_output = "Hello Ensign!"
     if msg == expected_output:
         print("Ok!")
+        remove_main("src/Main.class")
     else:
-        print("Error. The correct answer is `Hello World!`, but your output is:")
+        print("Error. The correct answer is `Hello Ensign!`, but your output is:")
         print("```")
         print(msg)
         print("```")
+        remove_main("src/Main.class")
